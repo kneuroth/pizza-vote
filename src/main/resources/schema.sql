6a2b50b97858579ddbe4aed7d4db2e32988e2197
kelly <kelly.neuroth@outlook.com>
2025-04-19 11:15:22 -0400

    Need if not exists

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE entries
+CREATE TABLE IF NOT EXISTS entries
 (
     id        int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name      varchar(255) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE entries
     pizzaYear int          NOT NULL
 );
 
-CREATE TABLE votes
+CREATE TABLE IF NOT EXISTS votes
 (
     id      int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name    varchar(255) NOT NULL,
